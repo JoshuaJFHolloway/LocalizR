788a271800be10d60c4361680dc52670b9d73e52
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-04-04 17:33:40 +0100

    Add QuizQuestion component, tests

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import QuizScenario from './QuizScenario';
 import QuizPicture from './QuizPicture';
+import QuizQuestion from './QuizQuestion';
 
 const Quiz = () => {
   return (
---
+++ src/components/QuizQuestion.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const QuizQuestion = () => {
+  return (
+    <p>This is our first Spanish question?</p>
+  )
+};
+
+export default QuizQuestion;
