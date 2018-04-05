6bc185cddd2b5b3ded64db5e67aaff8a3df455da
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-04-05 10:22:16 +0100

    Add QuizAnswer component, tests and pass

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import QuizScenario from './QuizScenario';
 import QuizPicture from './QuizPicture';
 import QuizQuestion from './QuizQuestion';
+import QuizAnswer from './QuizAnswer';
 
 const Quiz = () => {
   return (
