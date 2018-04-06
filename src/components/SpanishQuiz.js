77b8364df3ec0c2cadc1e7f6cdeae2d882ddc776
Joshua Holloway <josh.holloway@live.co.uk>
2018-04-06 23:14:46 +0100

    changed scenario string passed as prop slightly

--- src/components/SpanishQuiz.js
+++ src/components/SpanishQuiz.js
@@ -27,7 +27,7 @@ class SpanishQuiz extends Component {
     return (
       <div>
         <QuizScenario
-          scenario={"You are getting ready for your date"}
+          scenario={"You choose to wear this for your date"}
         />
         <QuizPicture
           picture={"https://www.opposuits.co.uk/media/catalog/product/cache/1/image/550x/925f46717e92fbc24a8e2d03b22927e1/s/s/ss_reddevil-lifestyle-01.jpg"}
