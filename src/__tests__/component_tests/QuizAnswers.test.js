af34acbb5d193bcbda1a5de3ad318f040cac016c
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-04-07 13:34:33 +0100

    Add test to count QuizAnswer components, pass

--- src/__tests__/component_tests/QuizAnswers.test.js
+++ src/__tests__/component_tests/QuizAnswers.test.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import QuizAnswers from '../../components/QuizAnswers';
+import QuizAnswer from '../../components/QuizAnswer';
 
 
 describe('QuizAnswers', () =>{
 
-    let quizanswers = shallow(<QuizAnswers />)
+  let quizanswers = shallow(<QuizAnswers />)
+  let mountquizanswers = mount(<QuizAnswers />)
 
-   describe('renders components correctly',() =>{
-     it('renders correctly', () => {
-       expect(quizanswers).toMatchSnapshot();    
-     });
+  describe('renders components correctly',() =>{
+    it('renders correctly', () => {
+      expect(quizanswers).toMatchSnapshot();
+    });
 
-     it('renders QuizAnswer', () =>{
-       expect(quizanswers.find('QuizAnswer').exists()).toBe(true);   
-     });
+    it('renders QuizAnswer', () =>{
+     expect(quizanswers.find('QuizAnswer').exists()).toBe(true);
+    });
 
-     it('renders QuizCorrectAnswer', () => {
-       expect(quizanswers.find('QuizCorrectAnswer').exists()).toBe(true);  
-     });
+    it('renders QuizCorrectAnswer', () => {
+      expect(quizanswers.find('QuizCorrectAnswer').exists()).toBe(true);
+    });
 
-   });
+    it('has 3 QuizAnswer components', () => {
+      expect(quizanswers.find('QuizAnswer').length).toEqual(3);
+    });
+
+  });
 });
 
 //Test incomplete, we don't know how to test the same component with different props
