a6ef369d737ab0fb5b84495718d9de5f42759b6e
jose <jennymarin1989@hotmail.com>
2018-04-07 11:13:33 +0100

    Add test for NameQuestion component and snapshots

---
+++ src/__tests__/component_tests/NameQuestion.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NameQuestion from '../../components/NameQuestion';
+
+describe('NameQuestion',() => {
+   
+  let namequestion = shallow(<NameQuestion />)
+  
+ describe('renders components correctly', () => {
+    
+    it('renders correctly',() =>{
+        expect(namequestion).toMatchSnapshot();
+    })
+ }) 
+})
