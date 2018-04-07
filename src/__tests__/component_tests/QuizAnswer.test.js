9a2fb03dea6f9e2914fafc6c9b34a8e5c39b3992
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-04-07 14:08:42 +0100

    Add QuizAnswer function call test for handleAnswerSubmitted function

--- src/__tests__/component_tests/QuizAnswer.test.js
+++ src/__tests__/component_tests/QuizAnswer.test.js
@@ -8,6 +8,15 @@ describe('QuizAnswer', () => {
   describe('Rendering components', () => {
     it('renders correctly', () => {
       expect(quizanswer).toMatchSnapshot();
+    });
+  });
+
+  describe('when user clicks QuizAnswer button', () => {
+    it('should call the handleAnswerSubmitted function', () => {
+      let mockhandleAnswerSubmitted = jest.fn();
+      let submit = shallow(<QuizAnswer handleAnswerSubmitted = {mockhandleAnswerSubmitted}/>);
+      submit.find("#submitButton").simulate('click');
+      expect(mockhandleAnswerSubmitted).toBeCalled();
     })
   })
-})
+});
