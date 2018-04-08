3e0bff501b036c52f78606f25d6170bf52a13ee0
jose <jennymarin1989@hotmail.com>
2018-04-08 23:34:13 +0100

    Create button for second spanish Scenario

--- src/components/SpanishScenarios.js
+++ src/components/SpanishScenarios.js
@@ -3,11 +3,17 @@ import SubmitButton from './SubmitButton'
 
 const SpanishScenarios = props => {
   return (
+    <div>
     <SubmitButton
       buttonName = {"Spanish Scenario 1"}
       handleDataSubmitted = {props.handleScenarioClicked}
     />
-  )
+    <SubmitButton
+      buttonName = {"Spanish Scenario 2"}
+      handleDataSubmitted = {props.handleScenarioClicked}
+    />
+    </div>
+  );
 };
 
 export default SpanishScenarios;
