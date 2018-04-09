44a1f6468a43af6ec839feae266f70744123c940
Kaari Strack <kaari.strack@gmail.com>
2018-04-09 18:46:52 +0100

    :robot: added a robot image for the button

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Picture = props => {
 
-    return (
-      <div>
-        <button onClick={props.handlePictureClicked}><img src="https://cdn2.iconfinder.com/data/icons/Flag/134/Spain.png" alt="themeimage" width="150"/> </button>
-      </div>
-    );
+    return <div>
+        <button onClick={props.handlePictureClicked}>
+          <img src="https://image.ibb.co/cv3YNH/if_spain_flag_683099.png" alt="themeimage" width="150" />{" "}
+        </button>
+      </div>;
 };
 
 export default Picture;
