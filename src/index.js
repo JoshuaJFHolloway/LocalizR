9948f69d4246dc473c29925e501ff4d1b59c4d90
Hannah Lillis <hannahamanda16@gmail.com>
2018-04-09 09:43:11 +0100

    Tidy index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import Routes from './routes';
-// import App from './components/App'
 
 render(
   <Routes />,
     document.getElementById('root')
 );
-
-// render(
-//     <CommentBox
-//         url="http://localhost:3001/api/comments"
-//         pollInterval={2000} />,
-//     document.getElementById('root')
-// );
