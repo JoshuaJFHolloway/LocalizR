2708b770b9ac326eeb9b4bb7fe0c30f474ae5934
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-04-10 00:09:26 +0100

    Add full test for 5 and 5 good ansers on Spanish scenario 1 and 2

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -12,6 +12,7 @@ describe('Slath', function () {
     cy.contains('Be a traveler, not a tourist')
   })
 
+// // // // this one had to be commented due to already havin' a registered account
   // it('can register and log in', function () {
   //   cy.contains('Log In or Sign Up').click()
   //   cy.contains('Sign Up').click()
@@ -42,6 +43,7 @@ describe('Slath', function () {
   it('can play scenario1-s first question', function () {
     cy.contains('Spanish Scenario 1').click()
     cy.contains('Log Out')
+    cy.get('img')
     cy.contains('You choose to wear this for your date')
     cy.contains('Which of these is your suit?')
     cy.contains('el vestido rojo')
@@ -52,6 +54,7 @@ describe('Slath', function () {
 
   it('can play scenario1-s second question', function () {
     cy.contains('Log Out')
+    cy.get('img')
     cy.contains("You're going to the restaurant where your date is waiting for you...")
     cy.contains('Which direction you should take?')
     cy.contains('Este')
@@ -60,22 +63,114 @@ describe('Slath', function () {
     cy.contains('Oeste').click()
   })
 
-  // it('can play scenario1-s third question', function () {
-  //
-  // })
+  it('can play scenario1-s third question', function () {
+    cy.contains('Log Out')
+    cy.get('img')
+    cy.contains('You want to surprise your date with some ....')
+    cy.contains('What is your idea of the perfect romantic gift?')
+    cy.contains('Galletas')
+    cy.contains('Cucharas')
+    cy.contains('Uvas')
+    cy.contains('Flores').click()
+  })
 
-  // it('can ')
+  it('can play scenario1-s fourth question', function () {
+    cy.contains('Log Out')
+    cy.get('img')
+    cy.contains('You want to impress your date with a nice compliment ....')
+    cy.contains('Which of these is the best compliment?')
+    cy.contains('Qué bello estás')
+    cy.contains('Qué extraña estás')
+    cy.contains('Qué grande estás')
+    cy.contains('Qué guapa estás').click()
+  })
 
-  // it('can register', function () {
-  //   cy.contains('Log In or Sign Up').click()
-  //   cy.get('input.auth0-lock-input').first()
-  //     .type('example@example.com')
-  //   cy.get('input.auth0-lock-input').last()
-  //     .type('password')
-  // })
+  it('can play scenario1-s fifth question', function () {
+    cy.contains('Log Out')
+    cy.get('img')
+    cy.contains("After a romantic and successfull date, you're inviting your date for a drink...")
+    cy.contains('Which of these is the best option?')
+    cy.contains('Mercado')
+    cy.contains('Banco')
+    cy.contains('Tienda')
+    cy.contains('Terraza-Bar de copas').click()
+  })
 
-  // it('can register user with e-mail and password', function () {
-  //   cy.contains('Log In or Sign Up').click().type('text')
-  // })
+  it('shows correct score after 5 good answers', function () {
+    cy.contains('Log Out')
+    cy.contains('Nice one, your score is 5')
+    cy.contains('Languages Page')
+    cy.contains('Retry Quiz')
+  })
 
+  it('Language Page button takes us back to home Choose Language page', function () {
+    cy.contains('Languages Page').click()
+    cy.contains('Choose your language!')
+  })
+})
+
+describe('Slath scenario 2', function () {
+  it('can play scenario2-s first question', function () {
+    cy.get('img').click()
+    cy.contains('Spanish Scenario 2').click()
+    cy.contains('Log Out')
+    cy.get('img')
+    cy.contains("After a long trip to Malaga, you're getting ready to go to the beach, but you don't know how to the direction")
+    cy.contains('Which of these is a swinsuit?')
+    cy.contains('Toalla')
+    cy.contains('Camiseta')
+    cy.contains('Calcetines')
+    cy.contains('Bañador').click()
+  })
+
+  it('can play scenario2-s second question', function () {
+    cy.contains('Log Out')
+    cy.get('img')
+    cy.contains('As a person who takes care of the skin, you need to buy sun protection to prevent sunburn..')
+    cy.contains('Which of these is the sun protector?')
+    cy.contains('Crema de cara')
+    cy.contains('Pasta de dientes')
+    cy.contains('Gel de baño')
+    cy.contains('Protector solar').click()
+  })
+
+  it('can play scenario2-s third question', function () {
+    cy.contains('Log Out')
+    cy.get('img')
+    cy.contains("Now, You're getting to the beach by Bus...")
+    cy.contains('Which bus you need to get?')
+    cy.contains('Dirección centro')
+    cy.contains('Dirección Feria')
+    cy.contains('Dirección pueblo')
+    cy.contains('Dirección playa').click()
+  })
+
+  it('can play scenario2-s fourth question', function () {
+    cy.contains('Log Out')
+    cy.get('img')
+    cy.contains("You're enjoying a nice day on the beach, but You're geting thristy...")
+    cy.contains('What can you get?')
+    cy.contains('Pan')
+    cy.contains('Botella de vino')
+    cy.contains('Ensalada')
+    cy.contains('Botella de agua').click()
+  })
+
+  it('can play scenario2-s fifth question', function () {
+    cy.contains('Log Out')
+    cy.get('img')
+    cy.contains('After a nice day on the beach, you lose the track time and you miss your bus ..')
+    cy.contains('What time is the next bus?')
+    cy.contains('Cinco y media')
+    cy.contains('Seis y media')
+    cy.contains('Ocho en punto')
+    cy.contains('Seis en punto').click()
+  })
+
+  it('shows correct score after 5 good answers', function () {
+    cy.contains('Log Out')
+    cy.contains('Nice one, your score is 5')
+    cy.contains('Languages Page')
+    cy.contains('Retry Quiz')
+  })
 })
