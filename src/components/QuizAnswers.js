3c93d83419d3ed6b5e61020c63370ff540ad636f
Joshua Holloway <josh.holloway@live.co.uk>
2018-04-10 09:31:48 +0100

    :sparkles: added randomiser on answers, but only randomises location of correct answer in two places

--- src/components/QuizAnswers.js
+++ src/components/QuizAnswers.js
@@ -1,24 +1,104 @@
-import React from 'react';
+import React, { Component } from 'react';
 import QuizAnswer from './QuizAnswer'
 import QuizCorrectAnswer from './QuizCorrectAnswer'
 
-const QuizAnswers = (props) => {
-  return (
-    <div>
-      <QuizAnswer
-        answer1={props.answer1}
-        answer2={props.answer2}
-        answer3={props.answer3}
-        handleAnswerSubmitted = {props.handleAnswerSubmitted}
-      />
-      <QuizCorrectAnswer
-        correctAnswer={props.correctAnswer}
-        handleCorrectAnswerSubmitted = {props.handleCorrectAnswerSubmitted}
-      />
-      
-    </div>
+  class QuizAnswers extends Component {
+    constructor(){
+      super();
+    }
 
-  )
-};
+    randomiser = (array= [1,2,3,4]) => {
+      for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+        var num1 = array[0];
+        return [num1];
+      };
+
+    decider = () => {
+      var values = this.randomiser();
+        if(values[0] === 1){
+          return 'Here1'
+        } if(values[0] === 2){
+          return 'Here2'
+      } if(values[0] === 3){
+        return 'Here3'
+      } if(values[0] === 4){
+        return 'Here4'
+      }
+    };
+
+    render() {
+
+      const Views = {
+        Here1: (
+          <div>
+            <QuizAnswer
+              answer1={this.props.answer1}
+              answer2={this.props.answer2}
+              answer3={this.props.answer3}
+              handleAnswerSubmitted={this.props.handleAnswerSubmitted}
+            />
+            <QuizCorrectAnswer
+              correctAnswer={this.props.correctAnswer}
+              handleCorrectAnswerSubmitted={this.props.handleCorrectAnswerSubmitted}
+            />
+          </div>
+        ),
+
+        Here2: (
+          <div>
+            <QuizCorrectAnswer
+              correctAnswer={this.props.correctAnswer}
+              handleCorrectAnswerSubmitted={this.props.handleCorrectAnswerSubmitted}
+            />
+            <QuizAnswer
+              answer1={this.props.answer1}
+              answer2={this.props.answer2}
+              answer3={this.props.answer3}
+              handleAnswerSubmitted={this.props.handleAnswerSubmitted}
+            />
+          </div>
+        ),
+
+        Here3: (
+          <div>
+            <QuizCorrectAnswer
+              correctAnswer={this.props.correctAnswer}
+              handleCorrectAnswerSubmitted={this.props.handleCorrectAnswerSubmitted}
+            />
+            <QuizAnswer
+              answer1={this.props.answer1}
+              answer2={this.props.answer2}
+              answer3={this.props.answer3}
+              handleAnswerSubmitted={this.props.handleAnswerSubmitted}
+            />
+          </div>
+        ),
+
+        Here4: (
+          <div>
+            <QuizAnswer
+              answer1={this.props.answer1}
+              answer2={this.props.answer2}
+              answer3={this.props.answer3}
+              handleAnswerSubmitted={this.props.handleAnswerSubmitted}
+            />
+            <QuizCorrectAnswer
+              correctAnswer={this.props.correctAnswer}
+              handleCorrectAnswerSubmitted={this.props.handleCorrectAnswerSubmitted}
+            />
+          </div>
+        )
+      };
+
+      return (
+        <div>
+          {Views[this.decider()]}
+        </div>
+      )
+    };
+}
 
 export default QuizAnswers;
