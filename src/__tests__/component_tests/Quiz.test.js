dce74503ee67713f2819f2e0a96db6b18059d396
Joshua Holloway <josh.holloway@live.co.uk>
2018-04-19 17:20:30 +0100

    :white_check_mark: tests for testing score changing correctly re-written for quiz.js

--- src/__tests__/component_tests/Quiz.test.js
+++ src/__tests__/component_tests/Quiz.test.js
@@ -23,6 +23,18 @@ describe('Quiz', () => {
       quiz.setState({ counter: 8 });
       expect(quiz.find('ResultsPage').exists()).toBe(true);
     });
-
   });
+
+  describe('Score updating correctly', () => {
+    it('does not change score when incorrect answer given', () => {
+      quiz.instance().handleAnswerSubmitted1();
+      expect(quiz.state('score')).toEqual(0)
+    });
+
+    it('increase the score by 1 when correct answer given', () => {
+      quiz.instance().handleCorrectAnswerSubmitted();
+      expect(quiz.state('score')).toEqual(1)
+    });
+  });
+
 });
