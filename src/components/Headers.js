311efba14e04bb241ef6220c634880de5b7dde8c
Joshua Holloway <josh.holloway@live.co.uk>
2018-08-01 12:05:20 +0100

    formatted better

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -4,14 +4,14 @@ import SubHeader from './SubHeader';
 import SubHeadLock from './styledComponents/subHeaderLock.js';
 
 const Headers = () => {
-  return (
-    <div>
-      <Header header={"LocalizR"} />
-      <SubHeadLock>
-      <SubHeader subHeader={"Be a traveler, not a tourist!"} />
-      </SubHeadLock>
-    </div>
-  )
+    return (
+        <div>
+            <Header header={"LocalizR"}/>
+            <SubHeadLock>
+                <SubHeader subHeader={"Be a traveler, not a tourist!"}/>
+            </SubHeadLock>
+        </div>
+    )
 };
 
 export default Headers;
