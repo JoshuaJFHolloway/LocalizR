ce02860bbe08da160d7be092f3f1b90ddccfb8a2
Joshua Holloway <josh.holloway@live.co.uk>
2018-08-01 12:05:38 +0100

    formatted better

--- src/components/LanguageList.js
+++ src/components/LanguageList.js
@@ -4,27 +4,27 @@ import SubHeader from './SubHeader';
 
 const LanguageList = (props) => {
 
-  return (
-    <div>
-      <SubHeader
-        subHeader={"Choose your language!"}
-      />
-      <Picture
-        handlePictureClicked={props.handleSpanishClicked}
-        flagImage={props.spanishImage}
-      />
-      <Picture
-        handlePictureClicked={props.handleRandomScenario}
-        flagImage={"https://image.ibb.co/dJADBc/la_questionmark.png"}
-      />
-      <Picture
-        handlePictureClicked={props.handleFrenchClicked}
-        flagImage={props.frenchImage}
-      />
-      {props.spanishScenarios}
-      {props.frenchScenarios}
-    </div>
-  );
+    return (
+        <div>
+            <SubHeader
+                subHeader={"Choose your language!"}
+            />
+            <Picture
+                handlePictureClicked={props.handleSpanishClicked}
+                flagImage={props.spanishImage}
+            />
+            <Picture
+                handlePictureClicked={props.handleRandomScenario}
+                flagImage={"https://image.ibb.co/dJADBc/la_questionmark.png"}
+            />
+            <Picture
+                handlePictureClicked={props.handleFrenchClicked}
+                flagImage={props.frenchImage}
+            />
+            {props.spanishScenarios}
+            {props.frenchScenarios}
+        </div>
+    );
 };
 
 export default LanguageList;
